d57d6e8934a44cfc34532fe740fadf019f56c056
hyemimi <hyemi7375@gmail.com>
2023-02-04 23:04:06 +0900

    feat : develop result page

--- my-quiz/src/routes/Pages/Result.tsx
+++ my-quiz/src/routes/Pages/Result.tsx
@@ -1,5 +1,24 @@
+import { useRecoilValue } from "recoil";
+import { CorrectState } from "../../atoms";
+import { Button } from "../../components/Quiz";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 function Result() {
-  return <div>퀴즈 결과 페이지입니다</div>;
+  const correct = useRecoilValue(CorrectState);
+  const navigate = useNavigate();
+  return (
+    <Div>
+      <h1>퀴즈 결과</h1>
+      <h3>10문제 중 {correct}문제를 맞추셨습니다! </h3>
+      <Button onClick={() => navigate("/")}>재도전!</Button>
+    </Div>
+  );
 }
+
+const Div = styled.div`
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+`;
 
 export default Result;
