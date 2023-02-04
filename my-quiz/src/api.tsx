d19ab5d44043cb825c606b0e90db2490c4999557
hyemimi <hyemi7375@gmail.com>
2023-02-04 17:46:39 +0900

    set api call function

---
+++ my-quiz/src/api.tsx
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export async function fetchQuestions() {
+  return await axios
+    .get("https://opentdb.com/api.php?amount=10")
+    .then((res) => res.data);
+}
--- my-quiz/src/routes/Main.tsx
+++ my-quiz/src/routes/Main.tsx
@@ -2,6 +2,8 @@ function Main() {
   return (
     <div>
       <h1>메인페이지입니다</h1>
+      <h3>h3확인</h3>
+      <caption>확인</caption>
     </div>
   );
 }
